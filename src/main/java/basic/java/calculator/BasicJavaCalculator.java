87d57ad7d45d391da03175be80d2adb9db82851e
Marco <ing.marcomaop@gmail.com>
2019-10-20 17:42:48 -0500

    Last Code Smells fixed

--- src/main/java/basic/java/calculator/BasicJavaCalculator.java
+++ src/main/java/basic/java/calculator/BasicJavaCalculator.java
@@ -1,24 +1,9 @@
 package basic.java.calculator;
 
-import basic.java.calculator.implementation.*;
-
 public class BasicJavaCalculator {
 
 	public static void main(String[] args) {
 		
-		/*
-		
-		IOperationCalculator operationcalculator = new OperationCalculator();
-		
-		Double firstNumber = 1.0;
-		Double secondNumber = 2.0;
-		
-		Double addition = operationcalculator.addition(firstNumber, secondNumber);
-		Double subtraction = operationcalculator.subtraction(firstNumber, secondNumber);
-		
-		System.out.println("Addition result: " + addition + "\nSubtraction result: " + subtraction);
-		
-		*/
 	}
 
 }
